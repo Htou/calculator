eb1560c0ee979069e6502077316943da342c5553
Hichem Touati <hichem@wearefrank.nl>
2023-06-20 13:10:49 +0200

    fixed error

--- src/main/java/Calculator/demo/calulator/CalculatorController.java
+++ src/main/java/Calculator/demo/calulator/CalculatorController.java
@@ -7,12 +7,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CalculatorController {
-        @Autowired
-        private Calculator calculator;
-        @RequestMapping("/sum")
-        String sum(@RequestParam("a") Integer a,
-                   @RequestParam("b") Integer b) {
-            return String.valueOf(calculator.sum(a, b));
-        }
+    @Autowired
+    private Calculator calculator;
+
+    @RequestMapping("/sum")
+    String sum(@RequestParam("a") Integer a,
+               @RequestParam("b") Integer b) {
+        return String.valueOf(calculator.sum(a, b));
     }
 }
